c309cfe859bf2a69ef3ae62bee2c0b52bf5b7029
Patrick Shaikh <patrick.shaikh@mayflower.de>
2022-06-29 00:24:35 +0200

    fix minor naming issues

--- infra/project/greengrass-core-provisioning.tf
+++ infra/project/greengrass-core-provisioning.tf
@@ -20,7 +20,10 @@ data "aws_iam_policy_document" "assume_greengrass_v2_token_exchange_role_policy"
     actions = ["sts:AssumeRole"]
     principals {
       type =  "AWS"
-      identifiers = [aws_iam_user.greengrass_user.arn, aws_iam_group.developers.arn]
+      identifiers = [
+        aws_iam_user.greengrass_user.arn,
+        #aws_iam_group.developers.arn
+      ]
     }
   }
 }
@@ -35,16 +38,15 @@ resource "aws_iam_access_key" "greengrass_user_access_key" {
 }
 
 resource "aws_iam_role" "greengrass_v2_token_exchange_role" {
-  name               = var.greengrass_v2_token_exchange_role_name
-  path               = "/system/"
+  name = var.greengrass_v2_token_exchange_role_name
   assume_role_policy = data.aws_iam_policy_document.assume_greengrass_v2_token_exchange_role_policy.json
   inline_policy {
-    name   = "GreengrassV2TokenExchangeRoleAccess"
+    name   = var.greengrass_v2_token_exchange_policy_name
     policy = data.aws_iam_policy_document.greengrass_v2_token_exchange_policy.json
   }
 }
 
 resource "aws_iot_role_alias" "alias" {
-  alias    = format("%sAlias",var.greengrass_v2_token_exchange_role_name)
+  alias    = var.greengrass_v2_token_exchange_role_alias
   role_arn = aws_iam_role.greengrass_v2_token_exchange_role.arn
 }
--- infra/project/variables.tf
+++ infra/project/variables.tf
@@ -8,6 +8,16 @@ variable "greengrass_v2_token_exchange_role_name" {
   description = "This is the role assumed for provisioning core devices."
 }
 
+variable "greengrass_v2_token_exchange_policy_name" {
+  type        = string
+  description = "This is the name of the provisioning role's policy."
+}
+
+variable "greengrass_v2_token_exchange_role_alias" {
+  type        = string
+  description = "This is the alias for the provisioning, which is used by AWS IoT to leverage other Amazon services."
+}
+
 variable "force_destroy_stack" {
   type        = bool
   default     = true
